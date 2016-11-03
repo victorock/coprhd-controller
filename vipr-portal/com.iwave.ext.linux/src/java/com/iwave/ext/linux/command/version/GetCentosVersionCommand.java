8e4cf4dad04eb69f504af2f003cda3b23abd85dd
Victor da Costa <victorockeiro@gmail.com>
2016-11-04 00:56:51 +0100

    COP-25113 ViPR support for CentOS 7.1.X

---
+++ vipr-portal/com.iwave.ext.linux/src/java/com/iwave/ext/linux/command/version/GetCentosVersionCommand.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012-2015 iWave Software LLC
+ * All Rights Reserved
+ */
+package com.iwave.ext.linux.command.version;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.iwave.ext.command.CommandException;
+import com.iwave.ext.linux.model.LinuxVersion;
+
+    private static final String PACKAGE_NOT_INSTALLED = "package centos-release is not installed";
+
+        setCommand("rpm -q --queryformat '%{VERSION}' centos-release");
+    }
+
+    @Override
+    public void parseOutput() {
+        String stdOut = getOutput().getStdout();
+        results = new LinuxVersion(LinuxVersion.LinuxDistribution.CENTOS, stdOut);
+    }
+
+    @Override
+    protected void processError() throws CommandException {
+        String stdout = StringUtils.trimToEmpty(getOutput().getStdout());
+        String stderr = StringUtils.trimToEmpty(getOutput().getStderr());
+        if (!StringUtils.equals(stdout, PACKAGE_NOT_INSTALLED) && !StringUtils.equals(stderr, PACKAGE_NOT_INSTALLED)) {
+            super.processError();
+        }
+    }
+}
